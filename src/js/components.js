da1a33be7e728fd52a2314a9d931ced590590226
julia.apakhova <julia.apakhova@yalantis.com>
2020-01-20 15:47:42 +0200

    landing - wip

--- src/js/components.js
+++ src/js/components.js
@@ -1,10 +1,10 @@
 (($) => {
   //toggle menu
   $('.js-nav-icon').click(function () {
-    $('.nav-global').addClass('nav-global--mobile nav-global--vertical');
+    $('.nav-landing').addClass('nav-global--mobile nav-global--vertical');
   });
   $('.js-nav-global__close').click(function () {
-    $('.nav-global').removeClass('nav-global--mobile nav-global--vertical');
+    $('.nav-landing').removeClass('nav-global--mobile nav-global--vertical');
   });
 
   //toggle card details
